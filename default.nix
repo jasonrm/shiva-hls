21459ca6e630b9cc9e1f16b7b5b95a3f61f0353f
Jason R. McNeil <jason@mcneil.dev>
2022-10-31 08:31:57 -0700

    nix: use buildGoModule

--- default.nix
+++ default.nix
@@ -1,13 +1,20 @@
-{
-  pkgs ? import <nixpkgs> {}
+{ pkgs ? import <nixpkgs> { }
 , lib ? pkgs.lib
-, makeScope ? lib.makeScope
-, newScope ? pkgs.newScope
+, buildGoModule ? pkgs.buildGoModule
 }:
-let
-  deps = import ./nix/deps.nix {};
-in
-pkgs.buildEnv {
-  name ="shiva-hls";
-  paths = with pkgs; [] ++ deps;
+buildGoModule rec {
+  pname = "shiva-hls";
+  version = "1.0.0";
+
+  src = ./.;
+
+  vendorSha256 = "sha256-owmyrctnL3p0uKjRhuOn0b/Wd59ZTUNrYwCufyLpMsc=";
+  # vendorSha256 = lib.fakeHash;
+
+  meta = with lib; {
+    description = "Download Twitch HLS streams";
+    license = licenses.mit;
+    homepage = "https://github.com/jasonrm/shiva-hls";
+    maintainer = [ "jason@mcneil.dev" ];
+  };
 }
